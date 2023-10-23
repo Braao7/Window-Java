b2166bb0a934ca4f4f5482c8396324f1ba693c7e
Braao7 <112507843+Braao7@users.noreply.github.com>
2023-10-23 12:44:48 -0300

    Add files via upload

---
+++ FirstWindows/src/main/java/com/mycompany/firstwindows/FirstWindows.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.firstwindows;
+
+/**
+ *
+ * @author ALUNO
+ */
+public class FirstWindows {
+
+    public static void main(String[] args) {
+        
+        Window window = new Window();
+        
+        window.setVisible(true);
+    }
+}
